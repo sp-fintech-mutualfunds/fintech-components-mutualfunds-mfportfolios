a60739c680254680b60219bcc795d2c74002f6c1
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2025-03-13 13:59:03 +1100

    Initial Commit

---
+++ Widgets.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Apps\Fintech\Components\Mf\Portfolios;
+
+use System\Base\Providers\ModulesServiceProvider\Modules\Components\ComponentsWidgets;
+
+class Widgets extends ComponentsWidgets
+{}
