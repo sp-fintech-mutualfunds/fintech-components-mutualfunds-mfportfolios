371a31acc87565f1dd776303d4e70e0550e546d4
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2025-03-15 00:53:02 +1100

    update

--- PortfoliosComponent.php
+++ PortfoliosComponent.php
@@ -2,15 +2,34 @@
 
 namespace Apps\Fintech\Components\Mf\Portfolios;
 
+use Apps\Fintech\Packages\Accounts\Users\AccountsUsers;
+use Apps\Fintech\Packages\Adminltetags\Traits\DynamicTable;
+use Apps\Fintech\Packages\Mf\Amcs\MfAmcs;
+use Apps\Fintech\Packages\Mf\Portfolios\MfPortfolios;
+use Apps\Fintech\Packages\Mf\Transactions\MfTransactions;
 use System\Base\BaseComponent;
 
 class PortfoliosComponent extends BaseComponent
 {
-    protected $package;
+    use DynamicTable;
+
+    protected $mfPortfoliosPackage;
+
+    protected $mfAmcsPackage;
+
+    protected $mfTransactionsPackage;
+
+    protected $accountsUsersPackage;
 
     public function initialize()
     {
-        //$this->package = $this->usePackage(?::class);
+        $this->mfPortfoliosPackage = $this->usePackage(MfPortfolios::class);
+
+        $this->mfAmcsPackage = $this->usePackage(MfAmcs::class);
+
+        $this->mfTransactionsPackage = $this->usePackage(MfTransactions::class);
+
+        $this->accountsUsersPackage = $this->usePackage(AccountsUsers::class);
     }
 
     /**
@@ -18,7 +37,136 @@ class PortfoliosComponent extends BaseComponent
      */
     public function viewAction()
     {
-        return;
+        $this->view->currencySymbol = '$';
+        if (isset($this->access->auth->account()['profile']['locale_country_id'])) {
+            $country = $this->basepackages->geoCountries->getById((int) $this->access->auth->account()['profile']['locale_country_id']);
+
+            if ($country && isset($country['currency_symbol'])) {
+                $this->view->currencySymbol = $country['currency_symbol'];
+            }
+        }
+
+        if (isset($this->getData()['id'])) {
+            $users = $this->accountsUsersPackage->getAccountsUserByAccountId($this->access->auth->account()['id']);
+
+            if (!$users) {
+                $users = [];
+            }
+
+            $this->view->users = $users;
+
+            if ($this->getData()['id'] != 0) {
+                $this->view->amcs = $this->mfAmcsPackage->getAll()->mfamcs;
+
+                $portfolio = $this->mfPortfoliosPackage->getPortfolioById((int) $this->getData()['id']);
+
+                if (!$portfolio) {
+                    return $this->throwIdNotFound();
+                }
+
+                if (!$portfolio['timeline']) {
+                    $portfolio['timeline'] = [];
+                }
+
+                $portfolio['equity_balance'] =
+                    str_replace('EN_ ',
+                                '',
+                                (new \NumberFormatter('en_IN', \NumberFormatter::CURRENCY))
+                                    ->formatCurrency($portfolio['equity_balance'], 'en_IN')
+                    );
+                $portfolio['invested_amount'] =
+                    str_replace('EN_ ',
+                                '',
+                                (new \NumberFormatter('en_IN', \NumberFormatter::CURRENCY))
+                                    ->formatCurrency($portfolio['invested_amount'], 'en_IN')
+                    );
+
+                $portfolio['total_value'] =
+                    str_replace('EN_ ',
+                                '',
+                                (new \NumberFormatter('en_IN', \NumberFormatter::CURRENCY))
+                                    ->formatCurrency($portfolio['total_value'], 'en_IN')
+                    );
+                $portfolio['profit_loss'] =
+                    str_replace('EN_ ',
+                                '',
+                                (new \NumberFormatter('en_IN', \NumberFormatter::CURRENCY))
+                                    ->formatCurrency($portfolio['profit_loss'], 'en_IN')
+                    );
+
+                // if (count($portfolio['balances']) > 0) {
+                //     foreach ($portfolio['balances'] as &$balance) {
+                //         $balance['total_value'] =
+                //             str_replace('EN_ ',
+                //                         '',
+                //                         (new \NumberFormatter('en_IN', \NumberFormatter::CURRENCY))
+                //                             ->formatCurrency($balance['total_value'], 'en_IN')
+                //             );
+                //     }
+                // }
+                $this->view->portfolio = $portfolio;
+            }
+
+            $this->view->pick('portfolios/view');
+
+            return;
+        }
+
+        $conditions =
+            [
+                'conditions'                => '-|account_id|equals|' . $this->access->auth->account()['id'] . '&'
+            ];
+
+        $controlActions =
+            [
+                // 'disableActionsForIds'  => [1],
+                'actionsToEnable'       =>
+                [
+                    'edit'      => 'mf/portfolios',
+                    'remove'    => 'mf/portfolios/remove'
+                ]
+            ];
+
+        $replaceColumns =
+            function ($dataArr) {
+                if ($dataArr && is_array($dataArr) && count($dataArr) > 0) {
+                    foreach ($dataArr as $key => &$data) {
+                        if ($data['account_id'] !== $this->access->auth->account()['id']) {
+                            unset($dataArr[$key]);
+                        } else {
+                            $data['total_value'] =
+                                str_replace('EN_ ',
+                                            '',
+                                            (new \NumberFormatter('en_IN', \NumberFormatter::CURRENCY))
+                                                ->formatCurrency($data['total_value'], 'en_IN')
+                                );
+                            $data['profit_loss'] =
+                                str_replace('EN_ ',
+                                            '',
+                                            (new \NumberFormatter('en_IN', \NumberFormatter::CURRENCY))
+                                                ->formatCurrency($data['profit_loss'], 'en_IN')
+                                );
+                        }
+                    }
+                }
+
+                return $dataArr;
+            };
+
+        $this->generateDTContent(
+            package: $this->mfPortfoliosPackage,
+            postUrl: 'mf/portfolios/view',
+            postUrlParams: $conditions,
+            columnsForTable: ['account_id', 'name', 'total_value', 'profit_loss'],
+            withFilter : true,
+            columnsForFilter : ['name', 'total_value', 'profit_loss'],
+            controlActions : $controlActions,
+            dtNotificationTextFromColumn: 'name',
+            excludeColumns : ['account_id'],
+            dtReplaceColumns: $replaceColumns
+        );
+
+        $this->view->pick('portfolios/list');
     }
 
     /**
@@ -28,11 +176,12 @@ class PortfoliosComponent extends BaseComponent
     {
         $this->requestIsPost();
 
-        //$this->package->add{?}($this->postData());
+        $this->mfPortfoliosPackage->addPortfolio($this->postData());
 
         $this->addResponse(
-            $this->package->packagesData->responseMessage,
-            $this->package->packagesData->responseCode
+            $this->mfPortfoliosPackage->packagesData->responseMessage,
+            $this->mfPortfoliosPackage->packagesData->responseCode,
+            $this->mfPortfoliosPackage->packagesData->responseData ?? []
         );
     }
 
@@ -43,26 +192,64 @@ class PortfoliosComponent extends BaseComponent
     {
         $this->requestIsPost();
 
-        //$this->package->update{?}($this->postData());
+        $this->mfPortfoliosPackage->updatePortfolio($this->postData());
 
         $this->addResponse(
-            $this->package->packagesData->responseMessage,
-            $this->package->packagesData->responseCode
+            $this->mfPortfoliosPackage->packagesData->responseMessage,
+            $this->mfPortfoliosPackage->packagesData->responseCode,
+            $this->mfPortfoliosPackage->packagesData->responseData ?? []
         );
     }
 
-    /**
-     * @acl(name=remove)
-     */
-    public function removeAction()
+    public function addTransactionAction()
     {
         $this->requestIsPost();
 
-        //$this->package->remove{?}($this->postData());
+        $this->mfTransactionsPackage->addMfTransaction($this->postData());
 
         $this->addResponse(
-            $this->package->packagesData->responseMessage,
-            $this->package->packagesData->responseCode
+            $this->mfTransactionsPackage->packagesData->responseMessage,
+            $this->mfTransactionsPackage->packagesData->responseCode,
+            $this->mfTransactionsPackage->packagesData->responseData ?? []
+        );
+    }
+
+    public function updateTransactionAction()
+    {
+        $this->requestIsPost();
+
+        $this->mfTransactionsPackage->updateMfTransaction($this->postData());
+
+        $this->addResponse(
+            $this->mfTransactionsPackage->packagesData->responseMessage,
+            $this->mfTransactionsPackage->packagesData->responseCode,
+            $this->mfTransactionsPackage->packagesData->responseData ?? []
+        );
+    }
+
+    public function removeTransactionAction()
+    {
+        $this->requestIsPost();
+
+        $this->mfTransactionsPackage->removeMfTransaction($this->postData());
+
+        $this->addResponse(
+            $this->mfTransactionsPackage->packagesData->responseMessage,
+            $this->mfTransactionsPackage->packagesData->responseCode,
+            $this->mfTransactionsPackage->packagesData->responseData ?? []
+        );
+    }
+
+    public function recalculatePortfolioTransactionsAction()
+    {
+        $this->requestIsPost();
+
+        $this->mfTransactionsPackage->recalculatePortfolioTransactions($this->postData());
+
+        $this->addResponse(
+            $this->mfTransactionsPackage->packagesData->responseMessage,
+            $this->mfTransactionsPackage->packagesData->responseCode,
+            $this->mfTransactionsPackage->packagesData->responseData ?? []
         );
     }
 }
--- Widgets.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Apps\Fintech\Components\Mf\Portfolios;
-
-use System\Base\Providers\ModulesServiceProvider\Modules\Components\ComponentsWidgets;
-
-class Widgets extends ComponentsWidgets
-{}
